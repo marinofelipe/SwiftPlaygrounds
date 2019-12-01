0d3c81d146bffca5901eeac1bf3a22b7afaa7826
Felipe Marino <felipemarino91@gmail.com>
2019-12-01 20:14:25 +0100

    Add Swift 5 + Autoclosure base strcuture

---
+++ Swift/Swift-5.playground/Pages/Autoclosure.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+/*:
+ # Autoclosure
+
+ An autoclosure is a closure that is automatically created to wrap an expression that’s being passed as an argument to a function. It doesn’t take any arguments, and when it’s called, it returns the value of the expression that’s wrapped inside of it.
+
+ > Overusing autoclosures can make your code hard to understand. The context and function name should make it clear that evaluation is being deferred.
+
+ //TODO:
+
+## References / Bibliografy
+
+  //TODO:
+
+ */
+
+//: [Previous](@previous) |
+//: [Next](@next)
